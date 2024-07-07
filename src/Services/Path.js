7a06f80a1e5a3804e64a4d624a15cfca84fe2b5e
BearerOP <ankity1892003@gmail.com>
2024-07-07 20:09:58 +0530

    baseURL configured

--- src/Services/Path.js
+++ src/Services/Path.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Path = axios.create({
-    baseURL: 'http://localhost:10000', // Replace with your backend URL
+    baseURL: 'https://up-status.onrender.com', // Replace with your backend URL
     headers: {
         'Content-Type': 'application/json'
     }
