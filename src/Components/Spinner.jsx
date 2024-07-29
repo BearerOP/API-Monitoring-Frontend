70b317abae888ead93e245d184c393ebd699b83b
BearerOP <ankity1892003@gmail.com>
2024-07-29 20:20:49 +0530

    spinner component added

---
+++ src/Components/Spinner.jsx
@@ -0,0 +1,11 @@
+// Spinner.js
+import React from "react";
+import '../Css/Spinner.css'
+
+const Spinner = () => (
+  <div className="spinner-border" role="status">
+    <span className="sr-only">Loading...</span>
+  </div>
+);
+
+export default Spinner;
