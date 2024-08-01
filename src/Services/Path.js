685c7293016973d72726a4bfba6728ce62e87add
BearerOP <ankity1892003@gmail.com>
2024-08-02 01:26:32 +0530

    path updated

--- src/Services/Path.js
+++ src/Services/Path.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { getToken } from '@/Services/AuthService'; // Create a utility to get the token
 
 const Path = axios.create({
-    baseURL: 'http://localhost:10000', // Replace with your backend URL
-    // baseURL: 'https://up-status.onrender.com', // Replace with your backend URL
+    // baseURL: 'http://localhost:10000', // Replace with your backend URL
+    baseURL: 'https://up-status.onrender.com', // Replace with your backend URL
     headers: {
         'Content-Type': 'application/json'
     }
