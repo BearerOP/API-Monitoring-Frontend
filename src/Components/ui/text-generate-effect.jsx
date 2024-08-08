36ea3fcd2544e36ecba55ab45e7158d48476970d
BearerOP <ankity1892003@gmail.com>
2024-08-09 01:52:39 +0530

    landing page responsive modified

--- src/Components/ui/text-generate-effect.jsx
+++ src/Components/ui/text-generate-effect.jsx
@@ -10,44 +10,42 @@ export const TextGenerateEffect = ({
   duration = 0.5
 }) => {
   const [scope, animate] = useAnimate();
-  let wordsArray = words.split(" ");
+  const wordsArray = words.split(" ");
+
   useEffect(() => {
+    // Animate the span elements when they are first rendered
     animate("span", {
       opacity: 1,
       filter: filter ? "blur(0px)" : "none",
     }, {
-      duration: duration ? duration : 1,
+      duration: duration || 1,
       delay: stagger(0.2),
     });
   }, [scope.current]);
 
   const renderWords = () => {
     return (
-      (<motion.div ref={scope}>
-        {wordsArray.map((word, idx) => {
-          return (
-            (<motion.span
-              key={word + idx}
-              className="text-white opacity-0"
-              style={{
-                filter: filter ? "blur(10px)" : "none",
-              }}>
-              {word}{" "}
-            </motion.span>)
-          );
-        })}
-      </motion.div>)
+      <motion.div ref={scope}>
+        {wordsArray.map((word, idx) => (
+          <motion.span
+            key={word + idx}
+            className="text-white opacity-0"
+            style={{
+              filter: filter ? "blur(10px)" : "none",
+            }}
+          >
+            {word}{" "}
+          </motion.span>
+        ))}
+      </motion.div>
     );
   };
 
   return (
-    (<div className={cn("font-bold", className)}>
-      <div className="">
-        <div
-          className=" text-white  text-9xl leading-snug tracking-tighter hover:tracking-normal transition-all duration-300">
-          {renderWords()}
-        </div>
+    <div className={cn("font-bold", className)}>
+      <div className="text-white text-2xl sm:text-4xl md:text-6xl lg:text-9xl leading-snug tracking-tight sm:tracking-normal transition-all duration-300">
+        {renderWords()}
       </div>
-    </div>)
+    </div>
   );
 };
