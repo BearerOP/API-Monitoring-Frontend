7b71501132aec4389264ba4409215f22de78d58b
BearerOP <ankity1892003@gmail.com>
2024-08-16 23:50:45 +0530

    landing page modifed

--- src/Screens/LandingPage.jsx
+++ src/Screens/LandingPage.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import Hero from '../Components/Hero.jsx';
 import Cards from '../Components/Cards.jsx';
-import Features from '../Components/Features.jsx';
-import Integration from '../Components/Integration.jsx';
-import Testimonials from '../Components/Testimonials.jsx';
-import CTA from '../Components/CTA.jsx';
 import Footer from '../Components/Footer.jsx';
 import Navbar from '../Components/Navbar.jsx';
 import '../Css/LandingPage.css';
 import { Toaster } from "@/Components/ui/toaster";
+import Testimonials from '@/Components/Testimonials.jsx';
 
 function LandingPage() {
   return (
@@ -17,10 +14,7 @@ function LandingPage() {
       <Navbar className='bg-[#0a0311]'/>
       <Hero />
       <Cards/>
-      <Features />
-      <Integration />
-      <Testimonials />
-      <CTA />
+      <Testimonials/>
       <Footer />
     </div>
       <Toaster />
