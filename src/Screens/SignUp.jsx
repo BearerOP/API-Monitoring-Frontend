74fc49bbd3c32318dd203629abe1b25cb55aec95
BearerOP <ankity1892003@gmail.com>
2024-08-16 23:38:39 +0530

    sign up page updated

--- src/Screens/SignUp.jsx
+++ src/Screens/SignUp.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Path from '../Services/Path';
-import svgImage from '../assets/images/undraw_sign_up_n6im.svg'; // Adjust the path if necessary
-import '../Css/SignUp.css';
+import { Input } from '@/Components/ui/input';
+import { Label } from '@/Components/ui/label';
+import { Button } from '@/Components/ui/button';
+import '../Css/SignUp.css'; // Ensure this path is correct
+import { WavyBackground } from '@/Components/ui/wavy-background';
 
 const SignUp = () => {
     const navigate = useNavigate();
@@ -12,7 +15,8 @@ const SignUp = () => {
     const [password, setPassword] = useState('');
     const [message, setMessage] = useState('');
 
-    const handleSignUp = async () => {
+    const handleSignUp = async (e) => {
+        e.preventDefault();
         setLoader(true);
         setMessage(''); // Clear previous messages
         try {
@@ -34,77 +38,102 @@ const SignUp = () => {
         }
     };
 
-    const isValidEmail = (email) => {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
-    };
+    const isValidEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    const isValidPassword = (password) => /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password);
 
-    const isValidPassword = (password) => {
-        const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-        return passwordRegex.test(password);
-    };
+    const isFormValid = () => username && isValidEmail(email) && isValidPassword(password);
 
-    const isFormValid = () => {
-        return username && isValidEmail(email) && isValidPassword(password);
-    };
+    const passwordErrorClass = !isValidPassword(password) && password ? 'text-rose-800 text-sm' : 'text-muted-foreground text-sm';
 
     return (
-        <div className="sign-up-container">
-            <div className="sign-up-card">
-                <h1 className="sign-up-title">Sign Up to UpStatus</h1>
-                <p className="sign-up-subtitle">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint</p>
-                <div className="sign-up-form">
-                    <div className="input-group">
-                        <input
-                            required
-                            type="text"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            className="input"
-                        />
-                        <label className="user-label">Full name</label>
-                    </div>
-                    <div className="input-group">
-                        <input
-                            required
-                            type="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            className="input"
-                        />
-                        <label className="user-label">Email address</label>
-                        {!isValidEmail(email) && email && <p className="error-message">Please enter a valid email address.</p>}
-                    </div>
-                    <div className="input-group">
-                        <input
-                            required
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="input"
-                        />
-                        <label className="user-label">Password</label>
-                        {!isValidPassword(password) && password && <p className="error-message">Password must be at least 8 characters long, include a number and a special character.</p>}
-                    </div>
-                    <div className="checkbox-container">
-                        <label htmlFor="login" className="checkbox-label"> 
-                            Already have an account!&emsp;
-                            <a href="/login" className="link">click here</a>
-                        </label>
-                    </div>
-                    {message && (
-                        <div className="error-message" role="alert">
-                            {message}
+        <div className='relative dark'>
+            <div className="w-full lg:grid h-screen lg:grid-cols-2">
+                <div className="flex items-center justify-center py-12 z-10 bg-background/70 border-r">
+                    <form className="mx-auto grid w-[350px] gap-6" onSubmit={handleSignUp}>
+                        <div className="grid gap-2 text-center">
+                            <h1 className="text-3xl font-bold">Sign Up</h1>
+                            <p className="text-balance text-muted-foreground">
+                                Create your account to get started
+                            </p>
                         </div>
-                    )}
-                    <button onClick={handleSignUp} disabled={!isFormValid()} className="sign-up-button boton-elegante">
-                        Sign Up
-                    </button>
-                    
+                        <div className="grid gap-4">
+                            <div className="grid gap-2">
+                                <Label htmlFor="username">Full Name</Label>
+                                <Input
+                                    id="username"
+                                    type="text"
+                                    placeholder="John Doe"
+                                    required
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <Label htmlFor="email">Email</Label>
+                                <Input
+                                    id="email"
+                                    type="email"
+                                    placeholder="m@example.com"
+                                    required
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                                {!isValidEmail(email) && email && (
+                                    <p className="text-rose-800 text-sm">
+                                        Please enter a valid email address.
+                                    </p>
+                                )}
+                            </div>
+                            <div className="grid gap-2">
+                                <Label htmlFor="password">Password</Label>
+                                <Input
+                                    id="password"
+                                    type="password"
+                                    required
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                                <p className="text-muted-foreground text-sm">
+                                    Password must be at least 8 characters long, include a number, and a special character.*
+                                </p>
+                                <p className={passwordErrorClass}>
+                                    Ensure your password meets the required criteria.
+                                </p>
+                            </div>
+                            {message && (
+                                <div className="text-rose-800 text-sm" role="alert">
+                                    {message}
+                                </div>
+                            )}
+                            <Button
+                                type="submit"
+                                disabled={!isFormValid() || loader}
+                                className="w-full"
+                            >
+                                {loader ? (
+                                    <>
+                                        Signing Up
+                                        <span className="spinner"></span>
+                                    </>
+                                ) : (
+                                    'Sign Up'
+                                )}
+                            </Button>
+                            <Button variant="outline" className="w-full">
+                                Sign Up with Google
+                            </Button>
+                        </div>
+                        <div className="mt-4 text-center text-sm">
+                            Already have an account?{" "}
+                            <a href="/login" className="underline">
+                                Login
+                            </a>
+                        </div>
+                    </form>
                 </div>
-            </div>
-            <div className="illustration">
-                <img src={svgImage} alt="Sign Up Illustration" className="illustration-image" />
+                <div className="absolute inset-0 -z-20">
+                    <WavyBackground />
+                </div>
             </div>
         </div>
     );
