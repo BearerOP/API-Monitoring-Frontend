a0606c7259a91104cd716970e24f3b52f37ee6fb
BearerOP <ankity1892003@gmail.com>
2024-08-17 23:57:06 +0530

    minor modification

--- src/Layouts/DashboardLayout.jsx
+++ src/Layouts/DashboardLayout.jsx
@@ -55,7 +55,7 @@ const DashboardLayout = () => {
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <NavLink to='/' className="flex items-center gap-2 font-semibold">
-              <h1 className="text-2xl">UpStatus</h1>
+              <h1 className="text-2xl">Avadhi</h1>
             </NavLink>
             <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
               <Bell className="h-4 w-4" />
