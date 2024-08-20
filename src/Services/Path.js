32ec0be08c49462940debf3430e3bbb89cf29fd9
BearerOP <ankity1892003@gmail.com>
2024-08-21 00:43:30 +0530

    path modified

--- src/Services/Path.js
+++ src/Services/Path.js
@@ -1,33 +1,38 @@
-import axios from 'axios';
-import { getToken } from '@/Services/AuthService'; // Create a utility to get the token
+import axios from "axios";
+import { getToken } from "@/Services/AuthService"; // Create a utility to get the token
 
 // Determine baseURL based on environment
-const baseURL = import.meta.env.VITE_APP_ENV === 'development'
-    ? 'http://localhost:10000' // Development URL
-    : 'http://3.110.50.62:10000'; // Production URL
+const baseURL =
+  import.meta.env.VITE_APP_ENV === "development"
+    ? "http://localhost:10000" // Development URL
+    : "https://up-status.onrender.com";
+// : 'http://3.110.50.62:10000'; // Production URL
 
 const Path = axios.create({
-    baseURL,
-    headers: {
-        'Content-Type': 'application/json'
-    }
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 // Add a request interceptor to include the token in the headers only for certain requests
 Path.interceptors.request.use(
-    config => {
-        // Only add the token for endpoints that require it
-        if (!config.url.includes('/forgot-password') && !config.url.includes('/reset-password')) {
-            const token = getToken();
-            if (token) {
-                config.headers['Authorization'] = `Bearer ${token}`;
-            }
-        }
-        return config;
-    },
-    error => {
-        return Promise.reject(error);
+  (config) => {
+    // Only add the token for endpoints that require it
+    if (
+      !config.url.includes("/forgot-password") &&
+      !config.url.includes("/reset-password")
+    ) {
+      const token = getToken();
+      if (token) {
+        config.headers["Authorization"] = `Bearer ${token}`;
+      }
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export default Path;
