33398dcb0c226139b6b377e8cc12cbf833ff2565
BearerOP <ankity1892003@gmail.com>
2024-08-27 05:40:50 +0530

    back to home button added to login pag

--- src/Screens/Login.jsx
+++ src/Screens/Login.jsx
@@ -87,6 +87,34 @@ export default function Login() {
 
   return (
     <div className='dark'>
+    <button onClick={() => navigate("/")} className="absolute top-4 left-4 bg-slate-800 lg:block z-50 no-underline group cursor-pointer shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+                <span className="absolute inset-0 overflow-hidden rounded-full">
+                    <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+                </span>
+                <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+                    <svg
+                        className="rotate-180"
+                        fill="none"
+                        height="16"
+                        viewBox="0 0 24 24"
+                        width="16"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M10.75 8.75L14.25 12L10.75 15.25"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="1.5"
+                        />
+                    </svg>
+                    <span>
+                        Back to Home
+                    </span>
+
+                </div>
+                <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-violet-400/90 to-violet-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+            </button>
       <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px] dark">
         <div className="flex items-center justify-center py-12">
           <form className="mx-auto grid w-[350px] gap-6" onSubmit={handleSubmit}>
